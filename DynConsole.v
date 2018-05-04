114473aade5c68f6c05f6b6c9d82d68e34aa2d6b
Juan Manuel Rico <juanmard@gmail.com>
2018-05-04 20:54:29 +0200

    Se realizan pruebas sobre la dinámica del cursor.

--- DynConsole.v
+++ DynConsole.v
@@ -25,8 +25,8 @@ module DynConsole
     input wire [7:0]  data_i,      // Data input.
 
     // Position cursor.
-    output reg [6:0]  cursor_x,     // X video map cursor position.
-    output reg [6:0]  cursor_y,     // Y video map cursor position.
+    output reg [6:0]  cursor_x = 1,     // X video map cursor position.
+    output reg [6:0]  cursor_y = 1,     // Y video map cursor position.
 
     // Video RAM interface.
     output reg  write,              // Write character in address video RAM.
@@ -47,33 +47,18 @@ begin
     // Dinámica del cursor.
     case (data_i)
         // Ctrl+j
-        8'h0A:
-        begin
-            cursor_x <= cursor_x - 1;
-            if (cursor_x < 0) cursor_x <= (640/size)-1;
-        end
-
+        8'h0A: cursor_x <= cursor_x - 1;
         // Ctrl+l
-        8'h0C:
-        begin
-            cursor_x <= cursor_x + 1;
-            if (cursor_x >= (640/size)-1) cursor_x <= 0;
-        end
-
+        8'h0C: cursor_x <= cursor_x + 1;
         // Ctrl+k
         8'h0B: cursor_y <= cursor_y + 1;
-
         // Ctrl+i
-        8'h09:
-        begin
-            cursor_y <= cursor_y - 1;
-            if (cursor_y < 0) cursor_y <= fin_pag-1;
-        end
+        8'h09: cursor_y <= cursor_y - 1;
 
         // Enter
         8'h0D, 8'h0A:
         begin
-            cursor_x <= 0;
+            cursor_x <= 1;
             cursor_y <= cursor_y + 1;
         end
 
@@ -81,7 +66,7 @@ begin
         8'h7F:
         begin
             cursor_x <= cursor_x - 1;
-            if (cursor_x < 0) cursor_x <= 0;
+            if (cursor_x < 0) cursor_x <= 1;
             character <= 0;
             write <= 1;
         end
@@ -95,11 +80,18 @@ begin
         end
     endcase
 
-    // Final de la página.
-    if (cursor_y > fin_pag)
+    // Final de línea.
+    if (cursor_x >= (640/size)-1)
     begin
-       cursor_y <= 0;
+        cursor_x <= 1;
+        cursor_y <= cursor_y + 1;
     end
+    
+    // if (cursor_x <= 0) cursor_x <= (640/size)-1;
+
+    // Final de la página.
+    // if (cursor_y >= fin_pag) cursor_y <= 1;
+    // if (cursor_y <= 0) cursor_y <= fin_pag-1;
 
 end
 
